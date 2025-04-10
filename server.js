4ae889e43dff92c0deef36c46b3059d9f8705658
finaxis002 <finaxis.ai@gmail.com>
2025-04-10 16:18:29 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,28 @@
+// index.js
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const connectDB = require("./Config/db")
+
+
+dotenv.config(); // Load .env
+
+
+const app = express();
+
+connectDB();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.get('/', (req, res) => {
+  res.send('Task Management Backend is running!');
+});
+
+// Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
