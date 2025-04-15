d6c483c44efb2f0c4823c8045c88a801c8248eb7
finaxis002 <finaxis.ai@gmail.com>
2025-04-15 11:36:04 +0530

    employee route changes

--- Models/Task.js
+++ Models/Task.js
@@ -1,17 +1,15 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
-const taskSchema = new mongoose.Schema({
+const TaskSchema = new mongoose.Schema({
   name: { type: String, required: true },
   due: { type: String, required: true },
   completed: { type: Boolean, default: false },
-  assignedTo: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Employee",
-    required: false,
+  assignee: {
+    name: { type: String },
+    email: { type: String }
   },
-  assignedToName: { type: String },
-  column: { type: Number },
-  createdAt: { type: Date, default: Date.now },
+  column: { type: String }, // For example: 'Recently assigned', 'Do today', etc.
+  createdAt: { type: Date, default: Date.now }
 });
 
-module.exports = mongoose.model("Task", taskSchema);
+module.exports = mongoose.model('Task', TaskSchema);
