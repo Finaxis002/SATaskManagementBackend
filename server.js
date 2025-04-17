a93aee390d30c3b9971d620121fd419b69043670
finaxis002 <finaxis.ai@gmail.com>
2025-04-17 18:33:51 +0530

    new changes

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const taskRoutes = require("./Routes/taskRoutes");
 const notificationRoutes = require("./Routes/notificationRoutes");
 const cookieParser = require("cookie-parser");
 const messageRoute = require("./Routes/messages");
-const {sendEmail} = require("./email/emailService")
+const path = require("path"); // <-- Import path
 
 
 dotenv.config(); // Load .env
@@ -36,6 +36,12 @@ app.use(
   })
 );
 
+app.use((req, res, next) => {
+  console.log(`Received request: ${req.method} ${req.url}`);
+  next();
+});
+
+
 app.use("/api/employees", emploeeRoutes);
 // app.use('/api/tasks', taskRoutes)
 app.use("/api/tasks", taskRoutes);
@@ -53,9 +59,14 @@ app.get("/", (req, res) => {
   res.send("Task Management Backend is running!");
 });
 
+// Serve static files from the React build folder
+app.use(express.static(path.join(__dirname, 'build')));
+
+// Catch-all handler to serve the React app for all routes
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
+
 
 
 
