28db755fbb37f194014979b1fd9d02353e41eee1
Finaxis002 <finaxis.ai@gmail.com>
2025-04-28 17:14:54 +0530

    reminder IST

--- services/taskReminderService.js
+++ services/taskReminderService.js
@@ -539,16 +539,16 @@ async function sendTaskReminder(task) {
 //     console.log("⏸️ Not reminder time. Skipping...");
 //     return;
 //   }
-const isMorningReminder = (currentHourIST === 13 && currentMinuteIST >= 55) || (currentHourIST === 14 && currentMinuteIST <= 5);
+const isMorningReminder = (currentHourIST === 10 && currentMinuteIST >= 55) || (currentHourIST === 11 && currentMinuteIST <= 5);
 const isEveningReminder = (currentHourIST === 16 && currentMinuteIST >= 55) || (currentHourIST === 17 && currentMinuteIST <= 5);
 const isSixPMReminder = (currentHourIST === 17 && currentMinuteIST >= 55) || (currentHourIST === 18 && currentMinuteIST <= 5);
-const isSixThirtyPMReminder = (currentHourIST === 18 && currentMinuteIST >= 25) && (currentMinuteIST <= 35);
+const isSixThirtyPMReminder = (currentHourIST === 15 && currentMinuteIST >= 25) && (currentMinuteIST <= 35);
 
 // Only proceed if it's the scheduled time
-// if (!(isMorningReminder || isEveningReminder || isSixPMReminder || isSixThirtyPMReminder)) {
-//   console.log("⏸️ Not reminder time. Skipping...");
-//   return;
-// }
+if (!(isMorningReminder || isEveningReminder || isSixPMReminder || isSixThirtyPMReminder)) {
+  console.log("⏸️ Not reminder time. Skipping...");
+  return;
+}
   // Now for each assignee
   task.assignees.forEach(assignee => {
     const assigneeEmail = assignee.email;
