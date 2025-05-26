324eb1157f335cfc8ca4faca41e0999ea027cf46
Finaxis002 <finaxis.ai@gmail.com>
2025-05-26 11:59:03 +0530

    leave changes

--- Models/Leave.js
+++ Models/Leave.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 
 const LeaveSchema = new mongoose.Schema({
   userId: { type: String, required: true }, // Add this field
+  userName: { type: String, required: true },
   fromDate: { type: Date, required: true },
   toDate: { type: Date, required: true },
   leaveType: { type: String, required: true },
